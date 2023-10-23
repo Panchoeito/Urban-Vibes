ceaa2c2b821b5fa64ad336b9021f6acd705b42f6
Josue Espinoza <jespinoza1812@gmail.com>
2023-10-23 13:35:09 -0300

    agregando pagina contacto

--- js/main.js
+++ js/main.js
@@ -22,3 +22,29 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
         icono_buscar.classList.remove("ocultar");
 })
+
+// validacion email pagina contacto, muestra mensaje de error solo si no es valido el formato
+
+document.getElementById("miFormulario").addEventListener("submit", function (e) {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  
+    // Obtén el valor del campo de correo electrónico
+    var emailInput = document.getElementById("email");
+    var email = emailInput.value;
+  
+    // Expresión regular para validar una dirección de correo electrónico
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  
+    // Realiza la validación
+    if (!emailPattern.test(email)) {
+      // Si la validación falla, muestra un mensaje de error
+      document.getElementById("errorEmail").textContent = "Dirección de correo electrónico no válida";
+      emailInput.classList.add("error");
+    } else {
+      // Si la validación es exitosa, borra cualquier mensaje de error
+      document.getElementById("errorEmail").textContent = "";
+      emailInput.classList.remove("error");
+  
+      
+    }
+  });
